afbe37b69bba199fea715f615f3d0987a6b7fc2f
wiisportsresort <wiisportsresort@users.noreply.github.com>
2021-06-05 03:52:12 +0000

    deploy: 6331678f229aa473e79af5984f0927ebfddbc13b

---
+++ _dist_/index.css.proxy.js
@@ -0,0 +1,23 @@
+if(typeof document!="undefined"){const e=`html,
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  overflow: hidden;
+}
+
+body {
+  background-color: black;
+
+  font-family: sans-serif;
+  color: white;
+}
+
+.game {
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+`,n=document.createElement("style"),t=document.createTextNode(e);n.type="text/css",n.appendChild(t),document.head.appendChild(n)}
